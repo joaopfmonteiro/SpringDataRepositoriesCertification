f360136928e6c767106f09798a10e129a72589fe
joaopfmonteiro <joaoosverdes@gmail.com>
2024-10-27 08:38:52 +0000

    Setup of the project

---
+++ src/main/java/com/example/SpringDataRepositoriesCertification/SpringDataRepositoriesCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataRepositoriesCertification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRepositoriesCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRepositoriesCertificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDataRepositoriesCertification/SpringDataRepositoriesCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataRepositoriesCertification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRepositoriesCertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
